45176a15dcda93c9948e6fa6da617270546efcc8
Leonardo-Luna <lunaleonardo031@gmail.com>
2024-08-09 15:21:17 -0300

    Controller sin usar DB, 'easter egg', segundo punto en el que la página funciona.

--- proyecto/src/Controller/ConferenceController.php
+++ proyecto/src/Controller/ConferenceController.php
@@ -14,12 +14,18 @@ class ConferenceController extends AbstractController
 {
 
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $greet = '';
+        if($name = $request->query->get('hello')) {
+            $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
+        }
+
         return new Response(<<<EOF
             <html>
                 <body>
-                    <img src="/images/under-construction.gif"/>
+                    $greet
+                    <img src="/images/under-construction.gif" />
                 </body>
             </html>
             EOF
